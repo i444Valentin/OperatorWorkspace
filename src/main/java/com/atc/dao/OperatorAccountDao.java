e8f9217cc89561ebb67b1bad0a07e38e9d292e03
Walentin <valession@gmail.com>
2021-12-06 23:05:15 +0300

    Реализация DAO класса OperatorAccountDao

--- src/main/java/com/atc/dao/OperatorAccountDao.java
+++ src/main/java/com/atc/dao/OperatorAccountDao.java
@@ -1,4 +1,49 @@
 package com.atc.dao;
 
+import com.atc.entity.AbonentsEntity;
+import com.atc.entity.OperatorAccountsEntity;
+import com.atc.utils.HibernateSessionFactoryUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class OperatorAccountDao {
-}
+    public OperatorAccountsEntity findById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(OperatorAccountsEntity.class, id);
+    }
+
+    public OperatorAccountsEntity findByLogin(String login) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(OperatorAccountsEntity.class, login);
+    }
+
+//    public void save(OperatorAccountsEntity opAccount) {
+//        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+//        Transaction transaction = session.beginTransaction();
+//        session.save(opAccount);
+//        transaction.commit();
+//        session.close();
+//    }
+
+//    public void update(OperatorAccountsEntity opAccount) {
+//        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+//        Transaction transaction = session.beginTransaction();
+//        session.update(opAccount);
+//        transaction.commit();
+//        session.close();
+//    }
+
+//    public void delete(OperatorAccountsEntity opAccount) {
+//        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+//        Transaction transaction = session.beginTransaction();
+//        session.delete(opAccount);
+//        transaction.commit();
+//        session.close();
+//    }
+
+    public List<OperatorAccountsEntity> findAll() {
+        return (List<OperatorAccountsEntity>)  HibernateSessionFactoryUtil
+                .getSessionFactory().openSession();
+    }
+
+    }
