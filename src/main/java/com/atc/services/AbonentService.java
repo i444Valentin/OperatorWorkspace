4262974d0f1b7eb3f00ea874c837dbd5184300bf
Walentin <valession@gmail.com>
2021-12-07 14:27:53 +0300

    Добавлен AbonentService

---
+++ src/main/java/com/atc/services/AbonentService.java
@@ -0,0 +1,39 @@
+package com.atc.services;
+
+import com.atc.dao.AbonentDao;
+import com.atc.entity.AbonentsEntity;
+
+import java.util.List;
+
+public class AbonentService {
+
+    AbonentDao abonentDao = new AbonentDao();
+
+    public AbonentsEntity findAbonent(int id) {
+        return abonentDao.findById(id);
+
+    }
+
+    public void saveAbonent(AbonentsEntity abonent) {
+        abonentDao.save(abonent);
+
+    }
+
+    public void updateAbonent(AbonentsEntity abonent) {
+        abonentDao.update(abonent);
+
+    }
+
+    public void deleteAbonent(AbonentsEntity abonent) {
+        abonentDao.delete(abonent);
+
+    }
+
+    public List<AbonentsEntity> findAllAbonents() {
+        return abonentDao.findAll();
+    }
+
+    public AbonentsEntity findAbonentWithName(String fname,String lname, String patronymic) {
+        return null;
+    }
+}
