7e0b1e79ddc8f6d6b039e63d080a0fee30cc801c
Walentin <valession@gmail.com>
2021-12-07 12:36:17 +0300

    Добавлена возможность входа для оператора

--- src/main/java/com/atc/сontrollers/Authorization.java
+++ src/main/java/com/atc/сontrollers/Authorization.java
@@ -1,9 +1,15 @@
 package com.atc.сontrollers;
 
+import com.atc.dao.OperatorAccountDao;
+import com.atc.entity.OperatorAccountsEntity;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.List;
 
 public class Authorization {
 
@@ -18,6 +24,17 @@ public class Authorization {
 
     @FXML
     void onHelloButtonClick(ActionEvent event) {
+        String login = loginTxtFld.getText();
+        String pass = passTxtFld.getText();
+        OperatorAccountDao operatorAccountDao = new OperatorAccountDao();
+        OperatorAccountsEntity operator = operatorAccountDao.findByLogin(login);
+        String dbLogin = operator.getLogin();
+        String dbPass = operator.getPassword();
+        if (dbLogin.equals(login) || dbPass.equals(pass)){
+            System.out.println("Authorization success on user " + dbLogin);
+            SceneLoader.load("/scenes/operator_workspace.fxml",false,true);
+            loginTxtFld.getScene().getWindow().hide();
+        }else System.out.println("Authorization failed on user " + dbLogin);
 
     }
 
