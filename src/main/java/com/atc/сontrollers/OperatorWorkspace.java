cc036ad9793a636540fb917f03e0962d982bf442
Walentin <valession@gmail.com>
2021-12-07 12:38:06 +0300

    Добавлен загрузчик сцен, Наполнение контроллера для рабочего места оператора

--- src/main/java/com/atc/сontrollers/OperatorWorkspace.java
+++ src/main/java/com/atc/сontrollers/OperatorWorkspace.java
@@ -1,4 +1,161 @@
 package com.atc.сontrollers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class OperatorWorkspace {
+
+    @FXML
+    private TabPane operatorWorkspaceTabs;
+
+    //Abonents Tab Section BEGIN
+    @FXML
+    private Tab AbonentsTab;
+
+    @FXML
+    private TableView<?> AbonentsTableView;
+
+    @FXML
+    private TableColumn<?, ?> abFiNameColumn;
+
+    @FXML
+    private TableColumn<?, ?> abLNameColumn;
+
+    @FXML
+    private TableColumn<?, ?> abPatronymicColumn;
+
+    @FXML
+    private TableColumn<?, ?> abBirthDateColumn;
+
+    @FXML
+    private TableColumn<?, ?> abGenderColumn;
+    //Abonents Tab Section END
+
+    //PhonesTab Section BEGIN
+    @FXML
+    private Tab PhonesTab;
+
+    @FXML
+    private TableView<?> PhonesTableView;
+
+    @FXML
+    private TableColumn<?, ?> phoneNumberColumn;
+
+    @FXML
+    private TableColumn<?, ?> phoneAbonentColumn;
+
+    @FXML
+    private TableColumn<?, ?> phoneTypeColumn;
+
+    @FXML
+    private TableColumn<?, ?> phoneTariffColumn;
+
+    @FXML
+    private TableColumn<?, ?> phoneDiscountColumn;
+
+    @FXML
+    private TableColumn<?, ?> idAbonentColumnH;
+
+    @FXML
+    private TableColumn<?, ?> phoneMountColumn;
+
+    @FXML
+    private TableColumn<?, ?> phoneMountTypeColumn;
+    //PhonesTab Section END
+
+    //TariffsTab Section BEGIN
+    @FXML
+    private Tab TariffsTab;
+
+    @FXML
+    private TableView<?> TariffsTableView;
+
+    @FXML
+    private TableColumn<?, ?> tariffNameColumn;
+
+    @FXML
+    private TableColumn<?, ?> tariffPriceColumn;
+
+    @FXML
+    private TableColumn<?, ?> tariffMinutesColumn;
+    //Tariffs Tab Section END
+
+    //RegPhonesTab Section BEGIN
+    @FXML
+    private Tab RegPhonesTab;
+
+    @FXML
+    private TableView<?> RegPhonesTableView;
+
+    @FXML
+    private TableColumn<?, ?> regOperatorColumn;
+
+    @FXML
+    private TableColumn<?, ?> regPhoneNumberColumn;
+
+    @FXML
+    private TableColumn<?, ?> regDateRegColumn;
+
+    @FXML
+    private TableColumn<?, ?> regOperatorIDColumnH;
+    //RegPhonesTab Section END
+
+    @FXML
+    private TextField searchTextTxtFld;
+
+    @FXML
+    private ImageView searchBtn;
+
+    @FXML
+    private ImageView searchCloseBtn;
+
+    @FXML
+    private Label currentOperatorLabel;
+
+    @FXML
+    void buttonClickCancelSearch(MouseEvent event) {
+
+    }
+
+    @FXML
+    void buttonClickDataExport(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonClickDataImport(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonClickDbSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonClickDbUpdate(ActionEvent event) {
+        System.out.println(operatorWorkspaceTabs.getSelectionModel().getSelectedItem().getId());
+
+    }
+
+    @FXML
+    void buttonClickGoSearch(MouseEvent event) {
+
+    }
+
+    @FXML
+    void keyEnterPressedGoSearch(KeyEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
 }
+
---
+++ src/main/java/com/atc/сontrollers/SceneLoader.java
@@ -0,0 +1,33 @@
+package com.atc.сontrollers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneLoader {
+    public static void load(String window, boolean isModality,boolean isResizable) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(SceneLoader.class.getResource(window));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setResizable(isResizable);
+        if (isModality) {
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+        } else {
+            stage.show();
+        }
+    }
+}
