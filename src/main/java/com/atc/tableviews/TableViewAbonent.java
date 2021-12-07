2fd09587343b93fe9f6e2e534e38d990a9f77567
Walentin <valession@gmail.com>
2021-12-07 20:12:07 +0300

    Добавлено скрытое поле id

--- src/main/java/com/atc/tableviews/TableViewAbonent.java
+++ src/main/java/com/atc/tableviews/TableViewAbonent.java
@@ -1,18 +1,34 @@
 package com.atc.tableviews;
 
+import java.util.Date;
+
 public class TableViewAbonent {
+    private Integer id;
     private String firstName;
     private String lastName;
     private String patronymic;
     private String birthDate;
     private String gender;
 
-    public TableViewAbonent(String fName, String lName, String patronymic, String birthDate, String gender) {
+    public TableViewAbonent(Integer id,String fName, String lName, String patronymic, String birthDate, String gender) {
+        this.id = id;
         this.firstName = fName;
         this.lastName = lName;
         this.patronymic = patronymic;
         this.birthDate = birthDate;
         this.gender = gender;
+    }
+
+    public TableViewAbonent() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -53,5 +69,9 @@ public class TableViewAbonent {
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    public boolean isNullFields() {
+        return firstName == null && lastName == null && patronymic == null && birthDate == null && gender == null;
     }
 }
