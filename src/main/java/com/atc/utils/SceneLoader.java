18fc55af2c22803d48e0e97687e69dca729194bc
Walentin <valession@gmail.com>
2021-12-08 20:45:27 +0300

    Теперь можно дать заголовок окну

--- src/main/java/com/atc/utils/SceneLoader.java
+++ src/main/java/com/atc/utils/SceneLoader.java
@@ -7,11 +7,12 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.function.Consumer;
 
 public class SceneLoader {
-    public static void load(String window, boolean isModality,boolean isResizable) {
+    public static void load(String fxmlFile, boolean isModality,boolean isResizable,String title) {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(SceneLoader.class.getResource(window));
+        loader.setLocation(SceneLoader.class.getResource(fxmlFile));
 
         try {
             loader.load();
@@ -22,6 +23,7 @@ public class SceneLoader {
         Parent root = loader.getRoot();
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
+        stage.setTitle(title);
         stage.setResizable(isResizable);
         if (isModality) {
             stage.initModality(Modality.APPLICATION_MODAL);
