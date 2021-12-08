9eb6f422f7b96a1266ece6a6f258090cfe3e5530
Walentin <valession@gmail.com>
2021-12-08 20:54:45 +0300

    Обновление информации о модуле

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.atc {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires validatorfx;
@@ -12,13 +11,22 @@ module com.atc {
     requires org.hibernate.orm.core;
     requires java.naming;
     requires org.hibernate.commons.annotations;
+    requires com.opencsv;
+    requires org.apache.commons.lang3;
     opens com.atc.tableviews to javafx.base;
-    opens com.atc to javafx.fxml, javafx.base;
     opens com.atc.entity to org.hibernate.orm.core;
+    opens com.atc.dao to org.hibernate.commons.annotations, org.hibernate.orm.core;
+    opens com.atc.сontrollers to javafx.fxml, javafx.controls, javafx.base, javafx.graphics, java.base, com.opencsv;
+    opens com.atc.utils to javafx.base, javafx.controls, javafx.fxml, javafx.graphics, com.opencsv, java.base;
+    opens com.atc to com.opencsv, java.base, javafx.base, javafx.controls, javafx.fxml, javafx.graphics;
+    opens com.atc.csvbeans to com.opencsv, java.base, javafx.base, javafx.controls, javafx.fxml, javafx.graphics;
+    opens com.atc.exporting to com.opencsv, java.base, javafx.base, javafx.controls, javafx.fxml, javafx.graphics;
     exports com.atc;
-    opens com.atc.dao to org.hibernate.commons.annotations,org.hibernate.orm.core;
-    opens com.atc.сontrollers to javafx.fxml, javafx.controls, javafx.base,javafx.graphics;
-    opens com.atc.utils to javafx.base, javafx.controls, javafx.fxml, javafx.graphics;
+    exports com.atc.utils;
+    exports com.atc.сontrollers;
+    exports com.atc.csvbeans;
+    exports com.atc.tableviews;
+    exports com.atc.exporting;
 
 
 }
