9822a3bcac73b7d79c580d65f3dd295fd940136f
Walentin <valession@gmail.com>
2021-12-08 20:55:24 +0300

    Добавлен класс для отображения телефонов в таблице

--- src/main/java/com/atc/tableviews/TableViewPhone.java
+++ src/main/java/com/atc/tableviews/TableViewPhone.java
@@ -1,4 +1,93 @@
 package com.atc.tableviews;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
 public class TableViewPhone {
+    private Integer number;
+    private String abonent;
+    private String phoneType;
+    private String tariff;
+    private Integer discount;
+    private Integer idAbonentH;
+    private String mount;
+    private String mountType;
+
+    public TableViewPhone(Integer number, String abonent, String phoneType, String tariff, Integer discount, Integer idAbonentH, String mount, String mountType) {
+        this.number = number;
+        this.abonent = abonent;
+        this.phoneType = phoneType;
+        this.tariff = tariff;
+        this.discount = discount;
+        this.idAbonentH = idAbonentH;
+        this.mount = mount;
+        this.mountType = mountType;
+    }
+
+    public TableViewPhone() {
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getAbonent() {
+        return abonent;
+    }
+
+    public void setAbonent(String abonent) {
+        this.abonent = abonent;
+    }
+
+    public String getPhoneType() {
+        return phoneType;
+    }
+
+    public void setPhoneType(String phoneType) {
+        this.phoneType = phoneType;
+    }
+
+    public String getTariff() {
+        return tariff;
+    }
+
+    public void setTariff(String tariff) {
+        this.tariff = tariff;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+
+    public Integer getIdAbonentH() {
+        return idAbonentH;
+    }
+
+    public void setIdAbonentH(Integer idAbonentH) {
+        this.idAbonentH = idAbonentH;
+    }
+
+    public String getMount() {
+        return mount;
+    }
+
+    public void setMount(String mount) {
+        this.mount = mount;
+    }
+
+    public String getMountType() {
+        return mountType;
+    }
+
+    public void setMountType(String mountType) {
+        this.mountType = mountType;
+    }
 }
