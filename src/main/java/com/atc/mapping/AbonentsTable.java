f2c9b21eae256cddea8ca45d54cf2ee1d464af9b
Walentin <valession@gmail.com>
2021-12-08 20:56:01 +0300

    Класс маппинга данных из таблицы БД в таблицу приложения

---
+++ src/main/java/com/atc/mapping/AbonentsTable.java
@@ -0,0 +1,42 @@
+package com.atc.mapping;
+
+import com.atc.entity.AbonentsEntity;
+import com.atc.tableviews.TableViewAbonent;
+
+import java.util.List;
+
+public class AbonentsTable {
+    private List<AbonentsEntity> entityList;
+    private List list;
+
+    public AbonentsTable(List<AbonentsEntity> entityList, List list) {
+        this.entityList =entityList;
+        this.list = list;
+    }
+
+     public List map() {
+        for (AbonentsEntity abonentsEntity : entityList) {
+
+            String gender;
+            if (abonentsEntity.isПол()) {
+                gender = "мужской";
+            } else {
+                gender = "женский";
+            }
+
+            String date;
+            date = abonentsEntity.getДатарождения().toString();
+            TableViewAbonent tableViewAbonent = new TableViewAbonent(
+                    abonentsEntity.getId(),
+                    abonentsEntity.getИмя(),
+                    abonentsEntity.getФамилия(),
+                    abonentsEntity.getОтчество(),
+                    date,
+                    gender);
+            list.add(tableViewAbonent);
+        }
+        return list;
+    }
+
+
+}
