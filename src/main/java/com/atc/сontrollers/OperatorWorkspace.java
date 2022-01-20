e18d9e4096b35b261ae29dd950092dd33782ef69
Walentin <valession@gmail.com>
2022-01-20 14:54:44 +0300

    Допилен контроллер окна оператора

--- src/main/java/com/atc/сontrollers/OperatorWorkspace.java
+++ src/main/java/com/atc/сontrollers/OperatorWorkspace.java
@@ -1,14 +1,20 @@
 package com.atc.сontrollers;
 
 import com.atc.entity.AbonentsEntity;
+import com.atc.entity.PhonesEntity;
+import com.atc.entity.RegistrationEntity;
+import com.atc.entity.TariffsEntity;
 import com.atc.exchange.Exchanger;
-import com.atc.mapping.AbonentsTable;
+import com.atc.mapping.AbonentsMapper;
+import com.atc.mapping.PhonesMapper;
+import com.atc.mapping.RegistrationMapper;
+import com.atc.mapping.TariffsMapper;
 import com.atc.services.AbonentService;
+import com.atc.services.PhoneService;
+import com.atc.services.RegPhoneService;
+import com.atc.services.TariffService;
 import com.atc.tableviews.*;
-import com.atc.exporting.ExportCSVAbonents;
 import com.atc.utils.SceneLoader;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -21,9 +27,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OperatorWorkspace extends Exchanger {
@@ -72,22 +77,24 @@ public class OperatorWorkspace extends Exchanger {
     private TableColumn<TableViewPhone, String> phoneAbonentColumn;
 
     @FXML
-    private TableColumn<TableViewPhone, ?> phoneTypeColumn;
+    private TableColumn<TableViewPhone, String> phoneTypeColumn;
 
     @FXML
-    private TableColumn<TableViewPhone, ?> phoneTariffColumn;
+    private TableColumn<TableViewPhone, String> phoneTariffColumn;
 
     @FXML
-    private TableColumn<TableViewPhone, ?> phoneDiscountColumn;
+    private TableColumn<TableViewPhone, Integer> phoneDiscountColumn;
 
     @FXML
-    private TableColumn<TableViewPhone, ?> idAbonentColumnH;
+    private TableColumn<TableViewPhone, Long> idAbonentColumnH;
 
     @FXML
-    private TableColumn<TableViewPhone, ?> phoneMountColumn;
+    private TableColumn<TableViewPhone, String> phoneMountColumn;
 
     @FXML
-    private TableColumn<TableViewPhone, ?> phoneMountTypeColumn;
+    private TableColumn<TableViewPhone, String> phoneMountTypeColumn;
+
+    ObservableList<TableViewPhone> phonesListTable;
     //PhonesTab Section END
 
     //TariffsTab Section BEGIN
@@ -98,13 +105,15 @@ public class OperatorWorkspace extends Exchanger {
     private TableView<TableViewTariff> TariffsTableView;
 
     @FXML
-    private TableColumn<TableViewTariff, ?> tariffNameColumn;
+    private TableColumn<TableViewTariff, String> tariffNameColumn;
 
     @FXML
-    private TableColumn<TableViewTariff, ?> tariffPriceColumn;
+    private TableColumn<TableViewTariff, BigDecimal> tariffPriceColumn;
 
     @FXML
-    private TableColumn<TableViewTariff, ?> tariffMinutesColumn;
+    private TableColumn<TableViewTariff, Integer> tariffMinutesColumn;
+
+    ObservableList<TableViewTariff> tariffsListTable;
     //Tariffs Tab Section END
 
     //RegPhonesTab Section BEGIN
@@ -115,16 +124,17 @@ public class OperatorWorkspace extends Exchanger {
     private TableView<TableViewRegistration> RegPhonesTableView;
 
     @FXML
-    private TableColumn<TableViewRegistration, ?> regOperatorColumn;
+    private TableColumn<TableViewRegistration, String> regOperatorColumn;
 
     @FXML
-    private TableColumn<TableViewRegistration, ?> regPhoneNumberColumn;
+    private TableColumn<TableViewRegistration, Long> regPhoneNumberColumn;
 
     @FXML
-    private TableColumn<TableViewRegistration, ?> regDateRegColumn;
+    private TableColumn<TableViewRegistration, String> regDateRegColumn;
 
     @FXML
-    private TableColumn<TableViewRegistration, ?> regOperatorIDColumnH; // H is hidden column
+    private TableColumn<TableViewRegistration, Integer> regOperatorIDColumnH; // H is hidden column
+    ObservableList<TableViewRegistration> registrationListTable;
     //RegPhonesTab Section END
 
     @FXML
@@ -140,11 +150,120 @@ public class OperatorWorkspace extends Exchanger {
     private Label currentOperatorLabel;
 
     @FXML
-    void initialize(){
+    private Label notFoundSearch;
+
+    @FXML
+    void initialize() {
         initializeAbonentsTableView();
         setEditableAbonentsTableView();
         getAbonents();
-        putInTableAbonents(AbonentsTableView,abonentsListTable);
+        putInTableAbonents(AbonentsTableView, abonentsListTable);
+
+        initializePhonesTableView();
+        setEditablePhonesTableView();
+        getPhones();
+        putInTablePhones(PhonesTableView, phonesListTable);
+
+        initializeTariffsTableView();
+        setEditableTariffsTableView();
+        getTariffs();
+        putInTableTariffs();
+
+        initializeRegistrationTableView();
+        getRegistration();
+        putInTableRegistration();
+    }
+
+    private void putInTableRegistration() {
+        RegPhonesTableView.setItems(registrationListTable);
+
+    }
+
+    private void getRegistration() {
+        RegPhoneService regPhoneService = new RegPhoneService();
+        List<RegistrationEntity> registrationEntities = regPhoneService.findAllRegistrations();
+        registrationListTable = FXCollections.observableArrayList();
+        RegistrationMapper registrationMapper = new RegistrationMapper(registrationEntities,registrationListTable);
+        registrationListTable = (ObservableList<TableViewRegistration>) registrationMapper.map();
+    }
+
+    private void initializeRegistrationTableView() {
+        regPhoneNumberColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        regOperatorColumn.setCellValueFactory(new PropertyValueFactory<>("operator"));
+        regDateRegColumn.setCellValueFactory(new PropertyValueFactory<>("regDate"));
+    }
+
+    private void putInTableTariffs() {
+        TariffsTableView.setItems(tariffsListTable);
+    }
+
+    private void getTariffs() {
+        TariffService tariffService = new TariffService();
+        List<TariffsEntity> tariffsEntities = tariffService.findAllTariffs();
+        tariffsListTable = FXCollections.observableArrayList();
+        TariffsMapper mapper = new TariffsMapper(tariffsEntities, tariffsListTable);
+        tariffsListTable = (ObservableList<TableViewTariff>) mapper.map();
+    }
+
+    private void setEditableTariffsTableView() {
+    }
+
+    private void initializeTariffsTableView() {
+        tariffNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tariffPriceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        tariffMinutesColumn.setCellValueFactory(new PropertyValueFactory<>("minutes"));
+
+        TariffsTableView.setRowFactory(param -> {
+            TableRow<TableViewTariff> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if ((event.getClickCount() == 1)) {
+                    //create a new Item and initialize it ...
+                    if (!tariffsListTable.get(tariffsListTable.size() - 1).isNullFields()) {
+                        TariffsTableView.getItems().add(new TableViewTariff());
+                    }
+                }
+            });
+            return row;
+        });
+    }
+
+    private void putInTablePhones(TableView<TableViewPhone> phonesTableView, ObservableList<TableViewPhone> phonesListTable) {
+        phonesTableView.setItems(phonesListTable);
+    }
+
+    private void getPhones() {
+        PhoneService phoneService = new PhoneService();
+        List<PhonesEntity> phonesList = phoneService.findAllPhones();
+        phonesListTable = FXCollections.observableArrayList();
+        PhonesMapper phonesMapper = new PhonesMapper(phonesList, phonesListTable);
+        phonesListTable = (ObservableList<TableViewPhone>) phonesMapper.map();
+        System.out.println();
+    }
+
+    private void setEditablePhonesTableView() {
+    }
+
+    private void initializePhonesTableView() {
+        phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<>("number"));
+        phoneAbonentColumn.setCellValueFactory(new PropertyValueFactory<>("abonent"));
+        phoneTypeColumn.setCellValueFactory(new PropertyValueFactory<>("phoneType"));
+        phoneTariffColumn.setCellValueFactory(new PropertyValueFactory<>("tariff"));
+        phoneDiscountColumn.setCellValueFactory(new PropertyValueFactory<>("discount"));
+        phoneMountColumn.setCellValueFactory(new PropertyValueFactory<>("mount"));
+        phoneMountTypeColumn.setCellValueFactory(new PropertyValueFactory<>("mountType"));
+
+        PhonesTableView.setRowFactory(param -> {
+            TableRow<TableViewPhone> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if ((event.getClickCount() == 1)) {
+                    //create a new Item and initialize it ...
+                    if (!phonesListTable.get(phonesListTable.size() - 1).isNullFields()) {
+                        PhonesTableView.getItems().add(new TableViewPhone());
+                    }
+                }
+            });
+            return row;
+        });
     }
 
     private void putInTableAbonents(TableView<TableViewAbonent> tableView, ObservableList<TableViewAbonent> observableList) {
@@ -154,38 +273,57 @@ public class OperatorWorkspace extends Exchanger {
 
     @FXML
     void buttonClickCancelSearch(MouseEvent event) {
+        String textSelectedTab = operatorWorkspaceTabs.getSelectionModel().getSelectedItem().getText();
+        searchCloseBtn.setVisible(false);
+        searchBtn.setVisible(true);
+        switch (textSelectedTab) {
+            case "Абоненты":
+                AbonentsTableView.setEditable(true);
+                AbonentService abonentService = new AbonentService();
+
+                List<AbonentsEntity> abonentsEntities = abonentService.findAllAbonents();
+                AbonentsMapper abonentsMapper = new AbonentsMapper(abonentsEntities, abonentsListTable);
+
+                abonentsListTable.clear();
+                AbonentsTableView.refresh();
+                abonentsListTable = (ObservableList<TableViewAbonent>) abonentsMapper.map();
+                AbonentsTableView.setItems(abonentsListTable);
+                notFoundSearch.setVisible(false);
+        }
 
     }
 
     @FXML
     void buttonClickDataExport(ActionEvent event) {
-        Exchanger.data = abonentsListTable;
-        SceneLoader.load("/scenes/export_data.fxml",true,false,"Экспорт данных");
+        List<Object> listTables = new ArrayList();
+        listTables.add(abonentsListTable);
+        listTables.add(phonesListTable);
+        listTables.add(tariffsListTable);
+        listTables.add(registrationListTable);
+        Exchanger.exportData = listTables;
+        SceneLoader.load("/scenes/export_data.fxml", true, false, "Экспорт данных", true);
 
 
     }
 
     @FXML
     void buttonClickDataImport(ActionEvent event) {
-        Path path = Paths.get("C:/Users/Walentin/Desktop/writtenBean.csv");
-        try {
-            ExportCSVAbonents exportCSV = new ExportCSVAbonents(path,null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (CsvRequiredFieldEmptyException e) {
-            e.printStackTrace();
-        } catch (CsvDataTypeMismatchException e) {
-            e.printStackTrace();
-        }
+        SceneLoader.load("/scenes/import_data.fxml", true, false, "Импорт данных", true);
+        abonentsListTable = (ObservableList<TableViewAbonent>) Exchanger.importData.get(0);
 
+        AbonentsTableView.refresh();
     }
 
     @FXML
     void buttonClickDbSave(ActionEvent event) {
         AbonentsTableView.sort();
         String selectedTabName = operatorWorkspaceTabs.getSelectionModel().getSelectedItem().getId();
-        switch(selectedTabName){
+        switch (selectedTabName) {
             case "AbonentsTab":
+                AlertMessage.showAlert(Alert.AlertType.INFORMATION,
+                        "Таблица " + operatorWorkspaceTabs.getSelectionModel().getSelectedItem().getText() +" была успешно сохранена.",
+                        "Сохранение таблицы",
+                        "");
                 System.out.println("Save table Abonents");
                 break;
             case "PhonesTab":
@@ -214,7 +352,29 @@ public class OperatorWorkspace extends Exchanger {
 
     @FXML
     void buttonClickGoSearch(MouseEvent event) {
+        searchBtn.setVisible(false);
+        searchCloseBtn.setVisible(true);
+        String textSelectedTab = operatorWorkspaceTabs.getSelectionModel().getSelectedItem().getText();
+        switch (textSelectedTab) {
+            case "Абоненты":
+                AbonentsTableView.setEditable(false);
+                AbonentService abonentService = new AbonentService();
+                String textInitials = searchTextTxtFld.getText();
+                String[] arrayInitials = textInitials.split(" ");
+                String textFirstName = arrayInitials[0];
+                String textLastName = arrayInitials[1];
+                String textPatronymic = arrayInitials[2];
+                List<AbonentsEntity> abonentsEntities = abonentService.findByInitials(textFirstName, textLastName, textPatronymic);
+                AbonentsMapper abonentsMapper = new AbonentsMapper(abonentsEntities, abonentsListTable);
 
+                abonentsListTable.clear();
+                AbonentsTableView.refresh();
+                abonentsListTable = (ObservableList<TableViewAbonent>) abonentsMapper.map();
+                AbonentsTableView.setItems(abonentsListTable);
+                if (AbonentsTableView.getItems().isEmpty()) {
+                    notFoundSearch.setVisible(true);
+                }
+        }
     }
 
     @FXML
@@ -225,7 +385,7 @@ public class OperatorWorkspace extends Exchanger {
     @FXML
     void logout(ActionEvent event) {
         searchTextTxtFld.getScene().getWindow().hide();
-        SceneLoader.load("/scenes/start.fxml",false,false,"Авторизация оператора");
+        SceneLoader.load("/scenes/start.fxml", false, false, "Авторизация оператора", false);
 
     }
 
@@ -246,8 +406,6 @@ public class OperatorWorkspace extends Exchanger {
                     }
                 }
             });
-            abFNameColumn.setOnEditCommit(event -> {
-            });
 
             return row;
         });
@@ -260,7 +418,7 @@ public class OperatorWorkspace extends Exchanger {
         AbonentService abonentService = new AbonentService();
         List<AbonentsEntity> abonentsList = abonentService.findAllAbonents();
         abonentsListTable = FXCollections.observableArrayList();
-        AbonentsTable abonentsTable = new AbonentsTable(abonentsList,abonentsListTable);
+        AbonentsMapper abonentsTable = new AbonentsMapper(abonentsList, abonentsListTable);
         abonentsListTable = (ObservableList<TableViewAbonent>) abonentsTable.map();
     }
 
